aff29a3c44ed3304472740623b223b060a32ead7
FabioBabaglioni <fabiobabaglioni@gmail.com>
2023-03-10 13:16:09 +0100

    Fix error

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -16,41 +16,46 @@ defineProps({
     <div class="container special">
 
         <nav class="py-3">
+            <div class="d-flex justify-content-between">
+                <a href="#">
+                    <!-- da inserire logo immagine -->
+                    logo
+                </a>
 
-            <div v-if="canLogin">
-                <Link v-if="$page.props.auth.user" :href="route('dashboard')">
-                Dashboard</Link>
+                <form class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
 
-                <template v-else>
-                    <div class="d-flex justify-content-between">
-                        <a href="#">
-                            <!-- da inserire logo immagine -->
-                            logo
-                        </a>
-
-                        <form class="d-flex" role="search">
-                            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                            <button class="btn btn-outline-success" type="submit">Search</button>
-                        </form>
-
-                        <div class="dropdown">
-                            <button id="btn_user" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                User
-                            </button>
-
-                            <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="#">
-                                        <Link :href="route('login')">
-                                        Log in</Link>
-                                    </a></li>
-                                <li><a class="dropdown-item" href="#">
-                                        <Link v-if="canRegister" :href="route('register')">
-                                        Register</Link>
-                                    </a></li>
-                            </ul>
+                <div class="dropdown">
+                    <button id="btn_user" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        User
+                    </button>
+                    <ul class="dropdown-menu">
+                        <div v-if="canLogin">
+                            <li>
+                                <a class="dropdown-item" href="#">
+                                    <Link v-if="$page.props.auth.user" :href="route('dashboard')">
+                                    Dashboard</Link>
+                                </a>
+                            </li>
                         </div>
-                    </div>
-                </template>
+                        <template v-else>
+                            <li>
+                                <a class="dropdown-item" href="#">
+                                    <Link :href="route('register')">
+                                    Register</Link>
+                                </a>
+                            </li>
+                            <li>
+                                <a class="dropdown-item" href="#">
+                                    <Link :href="route('login')">
+                                    Log in</Link>
+                                </a>
+                            </li>
+                        </template>
+                    </ul>
+                </div>
             </div>
         </nav>
 
@@ -59,13 +64,14 @@ defineProps({
                 <img src="./img/dl_a01597558.jpg" alt="">
                 <div class="pt-3">
                     <div class="fw-semibold">{{ apartment.title }}</div>
-                    <div>{{ apartment.address }}</div>
+                    <div>{{ apartment.adress }}</div>
                     <div>€ {{ apartment.price }}</div>
                 </div>
             </div>
         </div>
     </div>
 </template>
+
 
 <style lang="scss">
 @use "../../scss/app.scss" as *;
