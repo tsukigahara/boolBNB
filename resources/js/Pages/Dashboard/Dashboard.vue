2e980fa40a30afc2238b4c99496f53c4b482c828
tsukigahara <satsuki.tksm@gmail.com>
2023-03-10 17:21:13 +0100

    MyApartments styled with bootstrap

--- resources/js/Pages/Dashboard/Dashboard.vue
+++ resources/js/Pages/Dashboard/Dashboard.vue
@@ -16,7 +16,7 @@ defineProps({
 
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-
+                this is your dashboard
             </div>
         </div>
     </AuthenticatedLayout>
