ea0953fcc338c20bdf6914e26bd2a80a4c4bed50
francescoMain <cesaranofrancescomain@gmail.com>
2023-03-10 14:41:40 +0100

    edit

--- resources/js/Pages/Dashboard/ApartmentEdit.vue
+++ resources/js/Pages/Dashboard/ApartmentEdit.vue
@@ -1,12 +1,38 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-import { Head } from '@inertiajs/vue3';
-defineProps({
+import { Head, useForm } from '@inertiajs/vue3';
+
+const props = defineProps({
     apartment: Object,
     services: Array,
 });
-// console.log(apartment);
+
+
+
+const form = useForm({
+    title: props.apartment.title,
+    rooms:props.apartment.rooms,
+    beds:props.apartment.beds,
+    bathrooms:props.apartment.bathrooms,
+    square_meters:props.apartment.square_meters,
+    address:props.apartment.address,
+    latitude:props.apartment.latitude,
+    longitude:props.apartment.longitude,
+    main_image:props.apartment.main_image,
+    visible: props.apartment.visible,
+    price:props.apartment.price,
+    description: props.apartment.description,
+    services_id:props.apartment.services_id,
+});
+
+const submit = () => {
+    form.post(route('dashboard.apartments.store'), {
+        onFinish: () => form.reset(),
+    });
+};
 </script>
+
+
 
 <template>
     <Head title="My apartments" />
@@ -18,75 +44,69 @@ defineProps({
 
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <form class="d-flex" :action="route('dashboard.apartments.update', apartment.id)" method="POST">
+                <form class="d-flex" @submit.prevent="submit">
                     <div class="col">
                         <div class="mb-3">
                             <label for="" class="form-label">Title</label>
                             <input type="text" name="title" id="" class="form-control" placeholder=""
-                                aria-describedby="helpId" required>
+                                aria-describedby="helpId" v-model="form.title" required>
                         </div>
                         <div class="mb-3">
                             <label for="" class="form-label">Rooms</label>
                             <input type="number" name="rooms" id="" class="form-control" placeholder=""
-                                aria-describedby="helpId" required>
+                                aria-describedby="helpId" v-model="form.rooms" required>
                         </div>
                         <div class="mb-3">
                             <label for="" class="form-label">Beds</label>
                             <input type="number" name="beds" id="" class="form-control" placeholder=""
-                                aria-describedby="helpId" required>
+                                aria-describedby="helpId" v-model="form.beds" required>
                         </div>
                         <div class="mb-3">
                             <label for="" class="form-label">Bathrooms</label>
                             <input type="number" name="bathrooms" id="" class="form-control" placeholder=""
-                                aria-describedby="helpId" required>
+                                aria-describedby="helpId" v-model="form.bathrooms" required>
                         </div>
                         <div class="mb-3">
                             <label for="" class="form-label">Square meters</label>
                             <input type="number" name="square_meters" id="" class="form-control" placeholder=""
-                                aria-describedby="helpId" required>
+                                aria-describedby="helpId" v-model="form.square_meters" required>
                         </div>
                         <div class="mb-3">
                             <label for="" class="form-label">Address</label>
-                            <input type="number" name="address" id="" class="form-control" placeholder=""
-                                aria-describedby="helpId" required>
+                            <input type="text" name="address" id="" class="form-control" placeholder=""
+                                aria-describedby="helpId" v-model="form.address" required>
                         </div>
                         <div class="mb-3">
                             <label for="" class="form-label">latitude</label>
-                            <input type="number" name="latitude" id="" class="form-control" placeholder=""
-                                aria-describedby="helpId" required>
+                            <input type="text" name="latitude" id="" class="form-control" placeholder=""
+                                aria-describedby="helpId" v-model="form.latitude" required>
                         </div>
                         <div class="mb-3">
                             <label for="" class="form-label">longitude</label>
-                            <input type="number" name="longitude" id="" class="form-control" placeholder=""
-                                aria-describedby="helpId" required>
+                            <input type="text" name="longitude" id="" class="form-control" placeholder=""
+                                aria-describedby="helpId" v-model="form.longitude" required>
                         </div>
                         <div class="mb-3">
                             <label for="" class="form-label">Main image</label>
                             <input type="text" name="main_image" id="" class="form-control" placeholder=""
-                                aria-describedby="helpId" required>
-                        </div>
-                        <div class="mb-3">
-                            <label for="" class="form-label">visible</label>
-                            <input type="number" name="visible" id="" class="form-control" placeholder=""
-                                aria-describedby="helpId" required>
+                                aria-describedby="helpId" v-model="form.main_image" required>
                         </div>
                         <div class="mb-3">
                             <label for="" class="form-label">price</label>
-                            <input type="text" name="price" id="" class="form-control" placeholder=""
-                                aria-describedby="helpId" required>
+                            <input type="number" name="price" id="" class="form-control" placeholder=""
+                                aria-describedby="helpId" v-model="form.price" required>
                         </div>
                         <div class="mb-3">
                             <label for="" class="form-label">Description</label>
-                            <textarea name="description" id="" class="form-control" placeholder=""
-                                aria-describedby="helpId"></textarea>
+                            <textarea name="description" id="" class="form-control" placeholder="" aria-describedby="helpId"
+                                v-model="form.description"></textarea>
                         </div>
                         <div class="mb-3">
                             <label class="form-check-label" for="">SERVICES</label>
                             <div class="form-check">
                                 <div class="mb-3" v-for="service in services">
-                                    <input v-for="item in apartment.services" class="form-check-input" type="checkbox"
-                                        :value="service.id" id="" name="services[]"
-                                        :checked="item.id == service.id ? false : true">
+                                    <input v-for="item in apartment.services" :checked="item.id == service.id ? false : true" class="form-check-input" type="checkbox" :value="service.id" id=""
+                                        name="services_id[]" v-model="form.services_id">
                                     <label class="form-check-label" for="">{{ service.name }}</label>
                                 </div>
                             </div>
@@ -95,7 +115,6 @@ defineProps({
                             <button type="submit" class="btn btn-primary">Add</button>
                             <button type="reset" class="btn btn-warning">Reset</button>
                         </div>
-                        <input type="hidden" name="_token" :value="csrf">
                     </div>
                 </form>
             </div>
