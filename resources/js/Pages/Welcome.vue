cc9566c7249853dc9f38236189413481cbeda50b
FabioBabaglioni <fabiobabaglioni@gmail.com>
2023-03-11 10:48:35 +0100

    Aggiungo compontente Navbar

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { Head, Link } from '@inertiajs/vue3';
+import navBar from './navBar.vue'
 
 defineProps({
     apartments: Array,
@@ -11,42 +12,8 @@ defineProps({
 
     <div class="container h-100 vh-100">
 
-        <nav class="p-3">
-            <div class="d-flex justify-content-between">
-                <a href="#">
-                    <img class="logo" src="./img/580b57fcd9996e24bc43c513.png" alt="">
-                </a>
+        <navBar />
 
-                <form class="d-flex" role="search">
-                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-success" type="submit">Search</button>
-                </form>
-
-                <div class="dropdown">
-                    <button id="btn_user" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        menu
-                    </button>
-                    <ul class="dropdown-menu">
-                        <div v-if="$page.props.auth.user">
-                            <li>
-                                <Link class="dropdown-item" :href="route('dashboard')">
-                                Dashboard</Link>
-                            </li>
-                        </div>
-                        <template v-else>
-                            <li>
-                                <Link class="dropdown-item" :href="route('register')">
-                                Register</Link>
-                            </li>
-                            <li>
-                                <Link class="dropdown-item" :href="route('login')">
-                                Login</Link>
-                            </li>
-                        </template>
-                    </ul>
-                </div>
-            </div>
-        </nav>
         <div class="d-flex flex-wrap overflow-y-auto ms_padding">
             <div class="card_appa pt-2" v-for="apartment in apartments">
                 <a href="">
@@ -68,16 +35,6 @@ defineProps({
 <style lang="scss">
 @use "../../scss/app.scss" as *;
 @use "../../scss/general.scss" as *;
-
-.logo {
-    width: 100px;
-}
-
-#btn_user {
-    padding: 5px;
-    border-radius: 40px;
-    border: 1px solid grey;
-}
 
 .card_appa {
     width: calc((100% / 4) - 10px);
