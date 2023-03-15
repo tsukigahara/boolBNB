c4f7ea1bfb6534f33f5695c88cace7f7533d734f
FabioBabaglioni <fabiobabaglioni@gmail.com>
2023-03-15 17:52:22 +0100

    Creo finestra per sisultati search

--- resources/js/Pages/navBar.vue
+++ resources/js/Pages/navBar.vue
@@ -17,7 +17,7 @@ export default {
     },
     methods: {
 
-        searchApartments(element, range) {      
+        searchApartments(element, range) {
             store.advancedfApartments = [];
             const fullSearchAPI = `${store.searchAPI}/${element}/${range}`;
             axios.get(fullSearchAPI)
@@ -73,8 +73,8 @@ export default {
             }
         },
         searchAutocomplete(element) {
-             const fullAutocompleteAPI = `${store.autocompleteAPI}/${element}`;
-             axios.get(fullAutocompleteAPI)
+            const fullAutocompleteAPI = `${store.autocompleteAPI}/${element}`;
+            axios.get(fullAutocompleteAPI)
                 .then(res => {
                     store.autocompleteArray = res.data.response.data
                     console.log(store.autocompleteArray)
@@ -122,23 +122,23 @@ export default {
             </a>
 
             <div class="d-flex" role="search">
-                <input class="form-control" type="search" placeholder="Search" aria-label="Search"
-                    v-model="store.searchQuery"
-                    @input="checkInputLength()"
-                >
-                <div
-                    v-if="shouldMakeApiCall"
-                >
-                    <ul>
-                        <li v-for="elem in store.autocompleteArray.suggestions.results" @click="pickSuggestion(`${elem.address.freeformAddress}+${elem.address.countrySecondarySubdivision}+${elem.address.countrySubdivision}+${elem.address.country}`) ">
-                            {{ elem.address.freeformAddress }} | {{ elem.address.countrySecondarySubdivision }} | {{ elem.address.countrySubdivision }} | {{ elem.address.country }}
-                        </li>
-                    </ul>
+                <div class="position-relative">
+                    <input class="form-control search" type="search" placeholder="Search" aria-label="Search"
+                        v-model="store.searchQuery" @input="checkInputLength()">
+                    <div v-if="shouldMakeApiCall" class="campiRicerca">
+                        <ul>
+                            <li class="pulsante" v-for="elem in store.autocompleteArray.suggestions.results"
+                                @click="pickSuggestion(`${elem.address.freeformAddress}+${elem.address.countrySecondarySubdivision}+${elem.address.countrySubdivision}+${elem.address.country}`)">
+                                {{ elem.address.freeformAddress }} | {{ elem.address.countrySecondarySubdivision }} | {{
+                                    elem.address.countrySubdivision }} | {{ elem.address.country }}
+                            </li>
+                        </ul>
+                    </div>
                 </div>
-                <select class="form-control mx-2" name="radius" id="radius-select" v-model="store.searchRadius"
-                    v-if="store.isOnFiltered"
-                >
-                    <option value="" disabled selected>Seleziona raggio</option>
+
+                <select class="form-control " name="radius" id="radius-select" v-model="store.searchRadius"
+                    v-if="store.isOnFiltered">
+                    <option value="" disabled selected>raggio</option>
                     <option value="20">20km</option>
                     <option value="50">50km</option>
                     <option value="100">100km</option>
@@ -163,21 +163,20 @@ export default {
                     </div>
                 </div>
                 <input class="form-control mx-2" type="number" placeholder="Camere" aria-label="Rooms"
-                    v-model="store.searchRooms"
-                    v-if="store.isOnFiltered">
+                    v-model="store.searchRooms" v-if="store.isOnFiltered">
                 <input class="form-control me-2" type="number" placeholder="Letti" aria-label="Beds"
-                    v-model="store.searchBeds"
-                    v-if="store.isOnFiltered">
-                <button class="btn btn-outline-success" type="submit"
-                    @click.prevent="searchApartments(store.searchQuery, store.searchRadius)">Search</button>
+                    v-model="store.searchBeds" v-if="store.isOnFiltered">
+                <div>
+                    <button class="btn btn-outline-success mx-4" type="submit"
+                        @click.prevent="searchApartments(store.searchQuery, store.searchRadius)">Search</button>
+                </div>
+
                 <!-- <button @click.prevent="advancedSearchApartments(store.searchQuery, store.searchRadius)" class="btn btn-outline-success">AdS</button> -->
 
-                <a :href="route('filteredPage')" 
-                    v-if="!store.isOnFiltered"
-                >
-                    <button class="btn btn-outline-success">Advanced Search</button>
-                </a> 
-                
+                <a :href="route('filteredPage')" v-if="!store.isOnFiltered">
+                    <button class="btn btn-outline-success ">Advanced Search</button>
+                </a>
+
             </div>
 
             <div class="dropdown">
@@ -218,8 +217,26 @@ export default {
     border: 1px solid grey;
 }
 
+.search {
+    width: 400px;
+}
+
 .ms_btn {
     border: 1px solid grey;
+
+}
+
+.campiRicerca {
+    border: 1px solid grey;
+    border-radius: 20px;
+    width: 400px;
+    z-index: 30;
+    position: fixed;
+    background-color: white;
+}
+
+.pulsante:hover {
+    background-color: rgb(217, 217, 217);
 
 }
 </style>
