7dbe4ee7c2cb651b026fd07b3c4d9551dc1a8815
FabioBabaglioni <fabiobabaglioni@gmail.com>
2023-03-16 15:34:12 +0100

    Creo navbar funzionante

--- resources/js/Pages/Dashboard/dashboardNavbar.vue
+++ resources/js/Pages/Dashboard/dashboardNavbar.vue
@@ -1,5 +1,11 @@
 <script setup>
-
+import { ref } from 'vue';
+import ApplicationLogo from '@/Components/ApplicationLogo.vue';
+import Dropdown from '@/Components/Dropdown.vue';
+import DropdownLink from '@/Components/DropdownLink.vue';
+import NavLink from '@/Components/NavLink.vue';
+import ResponsiveNavLink from '@/Components/ResponsiveNavLink.vue';
+import { Link } from '@inertiajs/vue3';
 
 // props
 defineProps({
@@ -9,7 +15,35 @@ defineProps({
 </script>
 
 <template>
-    <nav class="navbar">
+    <nav class="navbar d-flex justify-content-between ">
+
+        <div class="container d-flex justify-content-between ">
+            <div>
+                <Link :href="route('welcome')">
+                <img class="logo" src="../../../js/Pages/img/580b57fcd9996e24bc43c513.png" alt="Airbnb">
+                </Link>
+            </div>
+
+            <div>
+                <li class="nav-item dropdown account">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                        aria-expanded="false">
+                        {{ $page.props.auth.user.name }}
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li>
+                            <DropdownLink :href="route('profile.edit')"> Profilo </DropdownLink>
+                        </li>
+                        <li>
+                            <DropdownLink :href="route('logout')" method="post">
+                                Log Out
+                            </DropdownLink>
+                        </li>
+
+                    </ul>
+                </li>
+            </div>
+        </div>
 
     </nav>
 </template>
@@ -21,7 +55,17 @@ defineProps({
 
 .navbar {
     height: 70px;
-    background-color: red;
+    // background-color: red;
     width: 100%;
+}
+
+.logo {
+    width: 100px;
+}
+
+.account {
+    border: 1px solid grey;
+    padding: 5px;
+    border-radius: 20px;
 }
 </style>
