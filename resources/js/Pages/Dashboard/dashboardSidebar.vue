e51fcbf93a12a3a7e6b2257e008c2a0a296dc776
FabioBabaglioni <fabiobabaglioni@gmail.com>
2023-03-16 15:10:42 +0100

    Creo strutturasidebar

---
+++ resources/js/Pages/Dashboard/dashboardSidebar.vue
@@ -0,0 +1,26 @@
+<script setup>
+
+
+// props
+defineProps({
+
+});
+
+</script>
+
+<template>
+    <div class="sidebar">
+
+    </div>
+</template>
+
+<style lang="scss">
+@use "../../../scss/app.scss" as *;
+@use "../../../scss/general.scss" as *;
+
+.sidebar {
+    width: 300px;
+    height: calc(100vh - 70px);
+    background-color: aquamarine;
+}
+</style>
