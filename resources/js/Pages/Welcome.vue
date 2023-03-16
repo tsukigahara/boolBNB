5d1f9e04fe30f4c209da70ff8a225f896d8162c6
DanieleAntonaci <danieleanto96@gmail.com>
2023-03-16 19:39:50 +0100

    Miglioramnto Card welcome

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -16,7 +16,7 @@ defineProps({
 
     <div class="container ">
 
-        <navBar />
+        <navBar class="position-sticky top-0 my_navbar"/>
 
         <div class="d-flex flex-wrap overflow-y-auto ms_padding container-md row row-cols-1 mx-auto">
 
@@ -26,10 +26,10 @@ defineProps({
                     <Link :href="route('show', apartment.id)">
                     <div class="pt-3 prova position-relative mb-2">
                         <img class="img card-img-top" src="./img/dl_a01597558.jpg" alt="">
-                        <div class="fw-semibold pt-2">{{ apartment.title }}</div>
-                        <div>{{ apartment.adress }}</div>
-                        <div>€ {{ apartment.price }}</div>
-                        <div class="position-absolute top-3 end-0 m-3 border border-2 rounded-pill px-2 my_sponsorship fw-bolder  " v-show="sponsorshipArray.includes(apartment.id)">Ann</div>
+                        <div class="fw-semibold pt-2 px-1 text-bold">{{ apartment.title }}</div>
+                        <div class="text-secondary pt-1 px-1 lh-1">{{ apartment.square_meters }} metri quadrati </div>
+                        <div class=" px-1 text-bold">{{ apartment.price }} &euro; <span class="text-secondary">notte</span> </div>
+                        <div class="position-absolute top-3 end-0 mx-2 mt-1 border border-2 rounded-bottom px-2 my_sponsorship fw-bolder  " v-show="sponsorshipArray.includes(apartment.id)">Sponsorizzato</div>
                     </div>
                     </Link>
                 </a>
@@ -46,7 +46,16 @@ defineProps({
 @use "../../scss/general.scss" as *;
 
 .my_sponsorship {
-    color: white;
+    color: $white;
+}
+
+.my_navbar {
+    z-index: 900;
+    background-color: $white;
+}
+
+:root {
+    scroll-behavior: smooth;
 }
 
 // .card_appa {
