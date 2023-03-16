c4bc2f3cacbbdd6923c72bcfbbc124a1d3bcdd8b
FabioBabaglioni <fabiobabaglioni@gmail.com>
2023-03-16 15:07:59 +0100

    Inserisco struttura navbar

---
+++ resources/js/Pages/Dashboard/dashboardNavbar.vue
@@ -0,0 +1,27 @@
+<script setup>
+
+
+// props
+defineProps({
+
+});
+
+</script>
+
+<template>
+    <nav class="navbar">
+
+    </nav>
+</template>
+
+<style lang="scss">
+@use "../../../scss/app.scss" as *;
+@use "../../../scss/general.scss" as *;
+
+
+.navbar {
+    height: 70px;
+    background-color: red;
+    width: 100%;
+}
+</style>
