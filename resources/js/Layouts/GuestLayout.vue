2d8435bd0f8bb3ac3732a90346c5e12dbd58123b
FabioBabaglioni <fabiobabaglioni@gmail.com>
2023-03-21 16:30:09 +0100

    Cambio logo guest e login

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -7,20 +7,18 @@ import { Link } from '@inertiajs/vue3';
     <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
         <div>
             <Link href="/">
-                <img class="logo" src="../pages/img/580b57fcd9996e24bc43c513.png" alt="">
+            <img class="logo" src="../pages/img/bool.png" alt="">
             </Link>
         </div>
 
-        <div
-            class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg"
-        >
+        <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
             <slot />
         </div>
     </div>
 </template>
 
 <style>
-    .logo{
-        width: 10rem;
-    }
+.logo {
+    width: 20rem;
+}
 </style>
